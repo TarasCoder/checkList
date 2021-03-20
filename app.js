bf6a3ba1fb6be0a00a861b80c79257654b2e59fd
kaskad kaskad <kaskad@_bigmir.net>
2021-03-20 19:29:15 +0200

    done main logic

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-$(".btn").click(function() {
-   alert("hi all");
+let arr = [];
+$(".btn").click(function () {
+   let a = $(".inpt").val();
+   $(".list").append("<li>" + a + "</li>");
+   arr.push(a);
+   console.log(arr);
 });
