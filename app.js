18fd0f2185e1614312da460287fdff2ebffeaa3b
kaskad kaskad <kaskad@_bigmir.net>
2021-03-20 20:03:31 +0200

    added some features and stylize a bit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let arr = [];
 $(".btn").click(function () {
    let a = $(".inpt").val();
    $(".list").append("<li>" + a + "</li>");
+   $('.inpt').val('');
    arr.push(a);
    console.log(arr);
 });
